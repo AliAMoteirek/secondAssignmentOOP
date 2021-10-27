a655b36283f8e6308bc3ce311e41cda9e8dfc49a
Yasser ALdahwi <yassiraldahwi@gmail.com>
2021-10-27 15:16:02 +0200

    Created swap method

--- src/puzzleGame/PuzzleGame.java
+++ src/puzzleGame/PuzzleGame.java
@@ -2,14 +2,17 @@ package puzzleGame;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Arrays;
 import java.util.Collections;
 
-public class PuzzleGame extends JFrame {
+public class PuzzleGame extends JFrame implements ActionListener {
     JPanel gamePanel = new JPanel() ;
     JPanel topPanel = new JPanel() ;
     JButton[] button = new JButton[16] ;
     JButton newGameButton = new JButton("New Game");
+    JButton buttonClicked;
 
     public PuzzleGame() {
         gamePanel.setLayout(new GridLayout(4,4)) ;
@@ -19,7 +22,9 @@ public class PuzzleGame extends JFrame {
             if (i != button.length - 1) {
                 button[i].setText(Integer.toString(i + 1));
             }
+            button[i].addActionListener(this);
             gamePanel.add(button[i]) ;
+
         }
 
         shuffleButtons() ;
@@ -54,6 +59,32 @@ public class PuzzleGame extends JFrame {
         }
         gamePanel.revalidate();
         gamePanel.repaint();
+    }
+
+    public void swapButtons() {
+        String clickedText = buttonClicked.getText() ;
+        int clickedIndex = 0 ;
+        int emptyIndex = 0 ;
+        for (int i = 0; i < button.length; i++) {
+            if (button[i].getText().equals(clickedText)) {
+                clickedIndex = i;
+                break;
+            }
+        }
+
+        for (int i = 0 ; i < button.length; i++){
+            if(button[i].getText().equals("")){
+                emptyIndex = i ;
+                break ;
+            }
+        }
+        Collections.swap(Arrays.asList(button), clickedIndex, emptyIndex);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        buttonClicked = (JButton) e.getSource();
+        swapButtons() ;
     }
 
     public static void main(String[] args) {
