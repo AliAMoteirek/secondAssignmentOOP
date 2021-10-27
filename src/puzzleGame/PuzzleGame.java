b32c2a73d3feb368490ab3fea4bdefe322b4ed86
Yasser ALdahwi <yassiraldahwi@gmail.com>
2021-10-27 13:18:14 +0200

    Created PuzzleGame buttons

--- src/puzzleGame/PuzzleGame.java
+++ src/puzzleGame/PuzzleGame.java
@@ -1,8 +1,37 @@
 package puzzleGame;
 
-public class PuzzleGame {
+import javax.swing.*;
+import java.awt.*;
+
+public class PuzzleGame extends JFrame {
+    JPanel gamePanel = new JPanel() ;
+    JPanel topPanel = new JPanel() ;
+    JButton[] button = new JButton[16] ;
+    JButton newGameButton = new JButton("New Game");
+
+    public PuzzleGame() {
+        gamePanel.setLayout(new GridLayout(4,4)) ;
+
+        for (int i = 0 ; i < button.length ; i++) {
+            button[i] = new JButton();
+            if (i != button.length - 1) {
+                button[i].setText(Integer.toString(i + 1));
+            }
+            gamePanel.add(button[i]) ;
+        }
+
+
+        topPanel.add(newGameButton) ;
+        add(topPanel, "North") ;
+        add(gamePanel, "Center") ;
+
+        setSize(420,420) ;
+        setVisible(true) ;
+        setLocationRelativeTo(null) ;
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE) ;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Test");
-        System.out.println("Yasser Test");
+        new PuzzleGame() ;
     }
 }
