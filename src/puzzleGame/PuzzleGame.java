c8faf87a4de3ae399acebab0a19d074a2e9fb91c
Yasser ALdahwi <yassiraldahwi@gmail.com>
2021-10-27 12:57:49 +0200

    Yasser test

--- src/puzzleGame/PuzzleGame.java
+++ src/puzzleGame/PuzzleGame.java
@@ -3,6 +3,6 @@ package puzzleGame;
 public class PuzzleGame {
     public static void main(String[] args) {
         System.out.println("Test");
-        System.out.println("Ali Test");
+        System.out.println("Yasser Test");
     }
 }
