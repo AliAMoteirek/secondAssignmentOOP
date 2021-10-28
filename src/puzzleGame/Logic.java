6594b5b8be195e904abc840d3fe9d0a55c1f56cf
Yasser ALdahwi <yassiraldahwi@gmail.com>
2021-10-28 10:50:39 +0200

    add isGameSolvable()

--- src/puzzleGame/Logic.java
+++ src/puzzleGame/Logic.java
@@ -1,5 +1,18 @@
 package puzzleGame;
 
+import java.util.ArrayList;
+
 public class Logic {
+
+    public boolean isGameSolvable(ArrayList<Button> buttonsList) {
+        int countInversions = 0;
+        for (int i = 0; i < buttonsList.size() - 1 ; i++) {
+            for (int j = 0; j < i; j++) {
+                if (Integer.parseInt(buttonsList.get(j).getText()) > Integer.parseInt(buttonsList.get(i).getText()))
+                    countInversions++;
+            }
+        }
+        return countInversions % 2 == 0;
+    }
 
 }
